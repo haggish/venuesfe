112051da7027bf8c10e7022fae61e37b26a70d05
Mika Salminen <mika.o.salminen@gmail.com>
2014-04-21 23:16:38 +0200

    Initial Yeoman skeleton

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('venuesfeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
