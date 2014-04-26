56228e5df62136b7c651ac55604b2c2d4f5210ec
Mika Salminen <mika.o.salminen@gmail.com>
2014-04-26 14:48:18 +0200

    Naming views more sensibly

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,9 +12,9 @@ angular.module('spree', [
     'leaflet-directive'
 ]).
     config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/view1', {templateUrl: 'views/partial1.html',
+        $routeProvider.when('/pick', {templateUrl: '../views/pick.html',
             controller: 'Planner'});
         $routeProvider.when('/view2', {templateUrl: 'views/partial2.html',
             controller: 'MyCtrl2'});
-        $routeProvider.otherwise({redirectTo: '/view1'});
+        $routeProvider.otherwise({redirectTo: '/pick'});
     }]);
